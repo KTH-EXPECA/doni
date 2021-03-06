d6bdb831aa90b0c9221a653d41e39fa7ca30f1bf
Jason Anderson <jasonanderson@uchicago.edu>
2021-03-05 19:56:46 -0600

    Fixup GHA not understanding the package version
    Looks like having tox not build an sdist could be the problem.

--- doni/common/config.py
+++ doni/common/config.py
@@ -1,13 +1,13 @@
 from oslo_config import cfg
 import osprofiler.opts as profiler_opts
 
+from doni import __version__
+from doni import PROJECT_NAME
+
 
 def parse_args(argv, default_config_files=None):
     """Initialize configuration defaults from argv.
     """
-    from doni import __version__
-    from doni import PROJECT_NAME
-
     cfg.CONF(argv[1:],
              project=PROJECT_NAME,
              version=__version__,
