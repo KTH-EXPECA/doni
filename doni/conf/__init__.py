48cfddaa82b2ea05ef187a59bf5a05a5d6611108
Jason Anderson <jasonanderson@uchicago.edu>
2021-03-19 17:14:49 -0500

    Add rootwrap configuration

--- doni/conf/__init__.py
+++ doni/conf/__init__.py
@@ -1,9 +1,11 @@
 from oslo_config import cfg
 
 from doni.conf import default
+from doni.conf import utils
 from doni.conf import worker
 
 CONF = cfg.CONF
 
 CONF.register_opts(default.opts)
+CONF.register_opts(utils.opts)
 CONF.register_opts(worker.opts, group=worker.GROUP)
