dcbcd2b540bf5f2aeefbe68e3ab0615baf6531ed
Jason Anderson <jasonanderson@uchicago.edu>
2021-03-25 19:49:50 -0500

    Fix typo on root endpoint

--- doni/api/root.py
+++ doni/api/root.py
@@ -7,8 +7,10 @@ bp = Blueprint("root", __name__)
 def info():
     return {
         "name": "OpenStack Doni API",
-        "description": ("Doni is an OpenStack project for managing hardware"
-                        "enrollment and availability."),
+        "description": (
+            "Doni is an OpenStack project for managing hardware "
+            "enrollment and availability."
+        ),
         "default_version": "1.0",
         "versions": ["1.0"],
     }
