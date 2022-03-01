2e491858b541e3ba036d4484ad93bede6b9f6003
Jason Anderson <jasonanderson@uchicago.edu>
2022-02-28 18:35:11 -0600

    do not require device driver as it is defualted

--- doni/driver/worker/blazar/device.py
+++ doni/driver/worker/blazar/device.py
@@ -18,7 +18,6 @@ class BlazarDeviceWorker(BaseBlazarWorker):
         WorkerField(
             "blazar_device_driver",
             default="k8s",
-            required=True,
             description=(
                 "Which Blazar device driver plugin to use to make the device "
                 "reservable. Defaults to k8s."
