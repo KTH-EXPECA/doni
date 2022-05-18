0c370df38dfca9140a87eaa1b2f11afb631ac091
Jason Anderson <jasonanderson@uchicago.edu>
2022-05-18 11:40:59 -0500

    use automap for data migration
    unclear how this worked before (presumably it didn't?) - the default
    sqlalchemy table/column functions return namedtuples, which cannot
    be modified, hence this would throw attribute errors when setting
    the state_details field.

--- doni/db/alembic/versions/868606f1faff_migrate_blazar_worker_state.py
+++ doni/db/alembic/versions/868606f1faff_migrate_blazar_worker_state.py
@@ -6,8 +6,8 @@ Create Date: 2022-02-04 15:34:42.594220
 
 """
 from alembic import op
-from oslo_db.sqlalchemy import types as db_types
 import sqlalchemy as sa
+from sqlalchemy.ext.automap import automap_base
 
 
 # revision identifiers, used by Alembic.
@@ -16,21 +16,20 @@ down_revision = "f42680f49a77"
 branch_labels = None
 depends_on = None
 
-worker_task = sa.sql.table(
-    "worker_task",
-    sa.sql.column("uuid", sa.String(36)),
-    sa.sql.column("worker_type", sa.String(64)),
-    sa.sql.column("state_details", db_types.JsonEncodedDict()),
-)
+Base = automap_base()
 
 
 def upgrade():
     """Update 'host' blazar worker fields to generic 'resource' naming."""
     bind = op.get_bind()
     session = sa.orm.Session(bind=bind)
-    for wt in session.query(worker_task).filter(
-        worker_task.columns.worker_type == "blazar.physical_host"
-    ):  # type: worker_task
+
+    Base.prepare(autoload_with=bind)
+    WorkerTask = Base.classes.worker_task
+
+    for wt in session.query(WorkerTask).filter(
+        WorkerTask.worker_type == "blazar.physical_host"
+    ):
         state_details = wt.state_details
         if "blazar_host_id" in state_details:
             state_details["blazar_resource_id"] = state_details["blazar_host_id"]
@@ -46,8 +45,12 @@ def upgrade():
 def downgrade():
     bind = op.get_bind()
     session = sa.orm.Session(bind=bind)
-    for wt in session.query(worker_task).filter(
-        worker_task.columns.worker_type == "blazar.physical_host"
+
+    Base.prepare(autoload_with=bind)
+    WorkerTask = Base.classes.worker_task
+
+    for wt in session.query(WorkerTask).filter(
+        WorkerTask.worker_type == "blazar.physical_host"
     ):  # type: worker_task
         state_details = wt.state_details
         if "blazar_resource_id" in state_details:
